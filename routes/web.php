9aee645a4d6a265de47cea78d87ce8cce8fc5eb5
josegpmsud <solintperez@gmail.com>
2024-06-04 09:24:36 -0400

    iniciando creacion de blog

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,42 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');    
 });
+
+Route::get('/posts', function () {
+    
+    return "Aqui estaran todas las listas de los posts";
+});
+
+//el orden de las rutas si importa laravel la primera que coincida que encuentre es la que muesta
+Route::get('/posts/create', function () {
+    
+    return "Aqui se mostrara un formulario para crear un nuevo post";
+});
+
+/*
+Route::get('/posts/{post}', function ($post) {
+    
+    return "Este es el post {$post}";
+});
+
+Route::get('/posts/{post}/{category}', function ($post, $category) {
+    
+    return "Este es el post {$post} de la categoria {$category}";
+});
+*/
+//este optimisa las dos rutas anteriores
+Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
+    
+    if($category){
+        return "Este es el post {$post} de la categoria {$category}";
+    }
+    return "Este es el post {$post}";
+});
+
+//Get
+//Post
+//Patch
+//Put
+//Delete
